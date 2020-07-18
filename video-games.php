fd302aeb12988bd7debe69c294e5de75aa95041f
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2020-07-18 18:03:30 -0400

    switch get to post

--- video-games.php
+++ video-games.php
@@ -1,7 +1,7 @@
-$name = htmlspecialchars($_GET['name'])
-$has-sex = htmlspecialchars($_GET['has-sex']);
-$gore  = htmlspecialchars($_GET['gore']);
-$focus = htmlspecialchars($_GET['focus'])
+$name = htmlspecialchars($_POST['name'])
+$has-sex = htmlspecialchars($_POST['has-sex']);
+$gore  = htmlspecialchars($_POST['gore']);
+$focus = htmlspecialchars($_POST['focus'])
 if($has-sex == 'checked' || $gore == 'checked' || $focus == 'checked') {
    echo $name + "'s a bad video game!!!"
 }
