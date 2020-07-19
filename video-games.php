1dc85c25a92ded54e5e4d7fb27f601f47b11af79
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2020-07-19 12:06:25 -0400

    Update video-games.php

--- video-games.php
+++ video-games.php
@@ -1,3 +1,4 @@
+<?php
 $name = htmlspecialchars($_POST['name'])
 $has-sex = htmlspecialchars($_POST['has-sex']);
 $gore  = htmlspecialchars($_POST['gore']);
@@ -5,3 +6,4 @@ $focus = htmlspecialchars($_POST['focus'])
 if($has-sex == 'checked' || $gore == 'checked' || $focus == 'checked') {
    echo $name + "'s a bad video game!!!"
 }
+?php>
