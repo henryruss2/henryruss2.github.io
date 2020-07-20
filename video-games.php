e337cc2fdeb776606bfc4bc4a8e29d2249648d97
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2020-07-20 09:23:50 -0400

    correct syntax

--- video-games.php
+++ video-games.php
@@ -1,9 +1,8 @@
-<?php
-$name = htmlspecialchars($_POST['name'])
+<?php $name = htmlspecialchars($_POST['name'])
 $has-sex = htmlspecialchars($_POST['has-sex']);
 $gore  = htmlspecialchars($_POST['gore']);
 $focus = htmlspecialchars($_POST['focus'])
 if($has-sex == 'checked' || $gore == 'checked' || $focus == 'checked') {
    echo $name + "'s a bad video game!!!"
 }
-?php>
+?>
