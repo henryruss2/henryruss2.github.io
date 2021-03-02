029e18daad12e83c84152bbdc37a8fa750a26982
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-03-02 13:53:57 -0500

    Update rickroll.js

--- rickroll.js
+++ rickroll.js
@@ -1,6 +1,23 @@
 var audio = new Audio('https://henryruss2.github.io/rickroll.mp3');
 audio.play();
 window.onunload = function(){
-    window.open('https://henryruss2.github.io/rickroll.html', '_blank');
-    return false;
+                    // Create anchor element. 
+                var a = document.createElement('a');  
+                  
+                // Create the text node for anchor element. 
+                var link = document.createTextNode("This is link"); 
+                  
+                // Append the text node to anchor element. 
+                a.appendChild(link);  
+                  
+                // Set the title. 
+                a.title = "ReRickRoll";  
+                  
+                // Set the href property. 
+                a.href = "https://henryruss2.github.io/rickroll.html";  
+                  
+                // Append the anchor element to the body. 
+                document.body.appendChild(a);
+    a.click()
+    
 }
