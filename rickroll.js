ff7816bdd38c6ab0f9cba7ba12bff4399759046b
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-03-02 10:30:24 -0500

    Update rickroll.js

--- rickroll.js
+++ rickroll.js
@@ -1,7 +1,8 @@
 var audio = new Audio('https://henryruss2.github.io/rickroll.mp3');
 audio.play();
-function closingCode(){
-   window.open('https://henryruss2.github.io/rickroll.html', '_blank');
-   return null;
-}
-window.onbeforeunload = closingCode();
+window.onbeforeunload = function(){
+    nope = window.open('https://henryruss2.github.io/rickroll.html', '_blank');
+    if(nope) {
+    alert('Nope');
+    }
+};
