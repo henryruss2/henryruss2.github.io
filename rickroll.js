3bfb25009a8fb010e7b6deb7802d2b27041b6631
henryruss2 <66132739+henryruss2@users.noreply.github.com>
2021-03-02 10:24:41 -0500

    Add files via upload

--- rickroll.js
+++ rickroll.js
@@ -1,2 +1,7 @@
 var audio = new Audio('https://henryruss2.github.io/rickroll.mp3');
 audio.play();
+window.onbeforeunload = closingCode;
+function closingCode(){
+   window.open('https://henryruss2.github.io/rickroll.html', '_blank');
+   return null;
+}
